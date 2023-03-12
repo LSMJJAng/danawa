e4050e7f413b6ab0f2a329df0c977b9f982c1f5c
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-03-13 01:09:13 +0900

    Delete danawa_android directory

--- danawa_android/app/src/main/java/com/example/danawa/OnProductItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.danawa;
-
-import android.view.View;
-
-public interface OnProductItemClickListener {
-    public void onItemClick(ProductAdapter.ViewHolder holder, View view, int position);
-}
